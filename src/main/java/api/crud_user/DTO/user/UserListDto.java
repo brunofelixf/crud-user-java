d2349774bdf0abb332acb762cc74603e90c215d7
brunofelixf <brunofelixf@gmail.com>
2023-01-11 23:11:40 -0300

    refactor: ajust userListDto

--- src/main/java/api/crud_user/DTO/user/UserListDto.java
+++ src/main/java/api/crud_user/DTO/user/UserListDto.java
@@ -2,15 +2,24 @@ package api.crud_user.DTO.user;
 
 import api.crud_user.model.AddressModel;
 import api.crud_user.model.UserModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
-public record UserListDto(
-        String name,
-        LocalDate birthday,
-        Long id,
-        java.util.List<AddressModel> addressModels) {
+@Data
+@AllArgsConstructor
+public class UserListDto {
+    private final String name;
+    private final LocalDate birthday;
+    private final Long id;
+    private final List<AddressModel> addressModels;
+
+
     public UserListDto(UserModel userModel) {
-        this(userModel.getName(), userModel.getBirthday(), userModel.getId(), userModel.getAddressModels() );
+        this(userModel.getName(), userModel.getBirthday(), userModel.getId(), userModel.getAddressModels());
     }
+
 }
